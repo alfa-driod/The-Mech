335ee04746072b3a11b13dba3710e14283a93ebd
prince <princebig12345@gmail.com>
2022-02-16 10:47:36 +0000

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'The Mechanic',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
